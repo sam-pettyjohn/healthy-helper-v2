e5719b43ca03ea820365a10286f1cf2134859cbd
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 13:07:19 -0500

    udpated server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const routes = require("./routes");
 const app = express();
 
@@ -10,15 +9,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets (for heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
+// Add routes
 app.use(routes);
 
-// Connect to the Mongo DB
+// Connect to Mongo DB
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/healthyhelperv2", {
     useNewUrlParser: true
