6618608c42113e590941b209f7e729497528ee2f
adrianapvent <adriana.e.pena@gmail.com>
2022-08-02 16:23:59 -0500

    recipe schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+// recipe Schema
+const recipe = new Schema({
+    uri: String,
+    calories: String,
+    protein: String,
+    fat: String,
+    carb: String,
+    label: String,
+    url: String,
+    time: Number,
+    ingredients: [String],
+    image: String,
+    _id: false
+  });
