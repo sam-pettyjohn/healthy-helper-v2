8b70d7283837e01b0ead5e883fb7a8998c8f6e1a
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 13:11:20 -0500

    dotenv twilio info

---
+++ server/config.js
@@ -0,0 +1,11 @@
+// DONT FORGET !! SEND .ENV FILE TO GROUP
+
+require("dotenv").config();
+
+module.exports = {
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioNumber: process.env.TWILIO_NUMBER,
+  email: process.env.EMAIL,
+  password: process.env.PASSWORD
+};
