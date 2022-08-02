f03f452d7e75ab98a284579b4dc6cc9f0bb8bbde
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 13:24:58 -0500

    defined user POST routes

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -11,4 +11,12 @@ router.put("/meal/:user", userController.updateMeal);
 router.put("/settings/:user", userController.updateSettings);
 router.put("/remove_meal/:user", userController.removeMeal);
 
+// POST Routes
+router.post("/user", userController.createUser);
+router.post("/searching", userController.retrieveRecipes);
+router.post("/menu/:user", userController.updateMenu);
+router.post("/preferences", userController.createPreferences);
+router.post("/sms", userController.sendSMS);
+router.post("/email", userController.sendEmail);
+
 module.exports = router;
