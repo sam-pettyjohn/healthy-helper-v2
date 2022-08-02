0511e4150074a9b41399271e7ebb10a79f04ea55
adrianapvent <adriana.e.pena@gmail.com>
2022-08-02 08:53:43 -0500

    retrieve receipes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,3 +29,47 @@ module.exports = {
          })
          .catch(err => res.status(422).json(err));
     },
+    retrieveRecipes: function (req, res) {
+        let allergy = "";
+        let diet = "";
+
+        if (req.body.vegan === true) {
+            allergy += "&health=vegan";
+          }
+          if (req.body.vegetarian === true) {
+            allergy += "&health=vegetarian";
+          }
+          if (req.body.sugar_conscious === true) {
+            allergy += "&health=sugar-conscious";
+          }
+          if (req.body.peanut_free === true) {
+            allergy += "&health=peanut-free";
+          }
+          if (req.body.tree_nut_free === true) {
+            allergy += "&health=tree-nut-free";
+          }
+          if (req.body.alcohol_free === true) {
+            allergy += "&health=alcohol-free";
+          }
+          if (req.body.dietType) {
+            diet += "&diet=" + req.body.dietType;
+          }
+
+          const apiURL = "https://api.edamam.com/search?";
+          const apiKey = "&app_key=71473ff8952bb7da30bc7a2f30cb6e51";
+          const apiID = "&app_id=f7341c7d";
+          let searchRange =
+            "&from=" + req.body.fromNumber + "&to=" + req.body.toNumber;
+          let query = "q=" + req.body.searchQuery;
+          // console.log(apiURL + query + apiID + apiKey + searchRange + diet + allergy);
+
+          axios
+          .get(apiURL + query + apiID + apiKey + searchRange + diet + allergy)
+          .then(response => {
+            if (response.data.hits.length !==0) {
+              res.json(response.data);
+            } else {
+                res.send("Error");
+            }
+          });
+    },
