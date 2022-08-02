7b5334fecc857a9036569e1fee2b4a8941507fab
adrianapvent <adriana.e.pena@gmail.com>
2022-08-02 08:43:39 -0500

    controllers/create user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,3 +5,27 @@ let preferences;
 const twilio = require("twilio");
 const client = new twilio(config.accountSid, config.authToken);
 const nodemailer = require("nodemailer");
+
+module.exports = {
+    findAll: function(req, res) {
+      db.User.findOne({ email: req.params.user })
+        .then(dbModel => {
+          res.json(dbModel);
+        })
+        .catch(err => res.status(422).json(err));
+    },
+    createUser: function(req, res) {
+        db.User.findOne({ email: req.body.email })
+         .then(dbUser => {
+            if (!dbUser) {
+             db.User.create({
+                email: req.body.email,
+                preferences: preferences
+             });
+             res.json("new user was added");
+            } else {
+                res.json("existing user");
+            }
+         })
+         .catch(err => res.status(422).json(err));
+    },
