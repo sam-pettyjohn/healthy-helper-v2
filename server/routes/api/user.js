b53a225194034ca28423ca227c5a0f0a2039012e
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 13:24:10 -0500

    defined user PUT routes

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -5,7 +5,10 @@ const userController = require("../../controllers/userController");
 // GET Routes
 router.get("/:user", userController.findAll);
 
-
-
+// PUT Routes
+router.put("/:user", userController.updateFavorites);
+router.put("/meal/:user", userController.updateMeal);
+router.put("/settings/:user", userController.updateSettings);
+router.put("/remove_meal/:user", userController.removeMeal);
 
 module.exports = router;
