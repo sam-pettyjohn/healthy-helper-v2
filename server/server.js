128859ceaec49f208f7759f031d3a85e48ec394f
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 23:26:46 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -10,9 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../client/build/static"));
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build/static"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Add routes, both API and view
 app.use(routes);
