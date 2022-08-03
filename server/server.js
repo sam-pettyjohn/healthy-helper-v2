748ea7bf04cd65fe95c8e00b0ec41f3407d2afad
sam-pettyjohn <sammpj47@gmail.com>
2022-08-02 22:56:50 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
 }
 
 // Add routes, both API and view
